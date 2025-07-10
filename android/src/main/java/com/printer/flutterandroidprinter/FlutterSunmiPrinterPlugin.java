287ff13c3b8c621e977c2e02c96c740ed693e08a
dante-qr <dante.arias@cobrosqr.com>
2025-07-10 16:52:38 -0400

    chore: flutter upgrade to 3.32.0

--- android/src/main/java/com/printer/flutterandroidprinter/FlutterSunmiPrinterPlugin.java
+++ android/src/main/java/com/printer/flutterandroidprinter/FlutterSunmiPrinterPlugin.java
@@ -7,8 +7,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-
 
 public class FlutterSunmiPrinterPlugin implements FlutterPlugin, MethodCallHandler {
   private MethodChannel channel;
@@ -31,29 +29,12 @@ public class FlutterSunmiPrinterPlugin implements FlutterPlugin, MethodCallHandl
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutterandroidprinter");
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutterandroidprinter");
     channel.setMethodCallHandler(this);
     flutterSunmiPrinterModule = new FlutterSunmiPrinterModule();
     flutterSunmiPrinterModule.initAidl(flutterPluginBinding.getApplicationContext());
   }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It
-  // supports the old pre-Flutter-1.12 Android projects. You are encouraged to
-  // continue supporting plugin registration via this function while apps migrate
-  // to use the new Android APIs post-flutter-1.12 via
-  // https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith
-  // to keep them functionally equivalent. Only one of onAttachedToEngine or
-  // registerWith will be called depending on the user's project.
-  // onAttachedToEngine or registerWith must both be defined in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutterandroidprinter");
-    channel.setMethodCallHandler(new FlutterSunmiPrinterPlugin());
-    flutterSunmiPrinterModule = new FlutterSunmiPrinterModule();
-    flutterSunmiPrinterModule.initAidl(registrar.context());
-  }
-
+  
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals(RESET)) {
